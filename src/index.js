f52325043e3f5fa05e58b1a8019efc158ea13191
Ekaterina Matveyeva <grinteabox@gmail.com>
2020-10-01 15:19:20 +0300

    feat: done functions and tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let min = array[0];
+    array.forEach ((item, index) => {
+      if (array[index] < min) {
+        min = array [index];
+      }
+    })
+    return min;
+  }
 }
+  
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  } else {
+  let max = array[0];
+  array.forEach ((item, index) => {
+    if (array[index] > max) {
+      max = array [index];
+    }
+  })
+  return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avg = 0;
+  let sum = 0;
+  if(array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    for(let i = 0; i <= array.length - 1; ++i) {
+        sum += array[i];
+    }
+    return avg = sum / array.length;
+  } 
 }
